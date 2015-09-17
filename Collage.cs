1d48a2684f6db27653a58ed5f7638ae39ad526b8
Kevin Kays <kkays@vt.edu>
2015-09-17 04:08:56 -0400

    Started work on Collage class.  Constructor complete

---
+++ Collage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CollageMaker
+{
+    class Collage
+    {
+        private ImageMeta _baseImage;
+        private PartialImageMeta[] _baseImageCells;
+        private ImageMeta[] _cellImages;
+
+        private void SortCells()
+        {
+
+        }
+
+        public Collage(string baseImagePath, string[] cellImagePaths)
+        {
+            Bitmap baseBitmap = new Bitmap(baseImagePath);
+            this._baseImage = new ImageMeta(baseBitmap, baseImagePath);
+            this._cellImages = new PartialImageMeta[cellImagePaths.Length];
+            for (int i = 0; i < cellImagePaths.Length; i++)
+            {
+                this._cellImages[i] = new ImageMeta(new Bitmap(cellImagePaths[i]), cellImagePaths[i]);
+            }
+            int numCols = (int)((baseBitmap.Width / (float)baseBitmap.Height) * Math.Sqrt(cellImagePaths.Length));
+            int numRows = (int)((baseBitmap.Height / (float)baseBitmap.Width) * Math.Sqrt(cellImagePaths.Length));
+
+            // Correct rounding
+            while (numRows * numCols < cellImagePaths.Length)
+                numRows++;
+
+            this._baseImageCells = PartialImageMeta.ArrayFromImage(baseBitmap, baseImagePath, numRows, numCols);
+        }
+    }
+}
--- PartialImageMeta.cs
+++ PartialImageMeta.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 
 namespace CollageMaker
@@ -10,6 +6,29 @@ namespace CollageMaker
     class PartialImageMeta : ImageMeta
     {
         protected Point _start;
+
+        /// <summary>
+        /// Divides an image into equally spaced cells based on numRows and numCols.
+        /// </summary>
+        /// <param name="numRows">The number of rows of PartialImageMeta</param>
+        /// <param name="numCols">The number of cols of PartialImageMeta</param>
+        /// <returns></returns>
+        public static PartialImageMeta[] ArrayFromImage(Bitmap sourceImage, string sourceImagePath, int numRows, int numCols)
+        {
+            PartialImageMeta[] cells = new PartialImageMeta[numRows * numCols];
+            int cellWidth = sourceImage.Width / numCols;
+            int cellHeight = sourceImage.Height / numRows;
+            
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    cells[(i * numCols) + j] = new PartialImageMeta(sourceImage, sourceImagePath, new Point(j * cellWidth, i * cellHeight), new Size(cellWidth, cellHeight));
+                }
+            }
+
+            return cells;
+        }
 
         PartialImageMeta(Bitmap sourceImage, string path, Point start, Size size) : base(sourceImage.Clone(new Rectangle(start, size), sourceImage.PixelFormat))
         {
