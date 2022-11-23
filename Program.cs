3e9e8ca81d48f26de6c41fc2b9032d5174061ff8
kns98 <kns98@yahoo.com>
2022-11-23 16:02:36 +0800

    tweak

--- Program.cs
+++ Program.cs
@@ -36,20 +36,10 @@ namespace CollageMaker
 
             //BMP, GIF, EXIF, JPG, PNG, and TIFF
 
-            IEnumerable<FileInfo> fileList1 = info.GetFiles("*.bmp",
+            
+            IEnumerable<FileInfo> fileList1 = info.GetFiles("*.jpg",
                 SearchOption.AllDirectories);
-            IEnumerable<FileInfo> fileList2 = info.GetFiles("*.gif",
-                SearchOption.AllDirectories);
-            IEnumerable<FileInfo> fileList3 = info.GetFiles("*.exif",
-                SearchOption.AllDirectories);
-            IEnumerable<FileInfo> fileList4 = info.GetFiles("*.jpg",
-                SearchOption.AllDirectories);
-            IEnumerable<FileInfo> fileList5 = info.GetFiles("*.jpeg",
-               SearchOption.AllDirectories);
-            IEnumerable<FileInfo> fileList6 = info.GetFiles("*.png",
-                SearchOption.AllDirectories);
-            IEnumerable<FileInfo> fileList7 = info.GetFiles("*.tiff",
-                SearchOption.AllDirectories);
+
 
             IEnumerable<FileInfo> _fileQuery1 =
                 from file in fileList1
@@ -58,76 +48,13 @@ namespace CollageMaker
                 select file;
 
             var fileQuery1 = _fileQuery1.ToArray();
-
-            IEnumerable<FileInfo> _fileQuery2 =
-                from file in fileList2
-                where file.Length > 0
-                orderby file.Name
-                select file;
-
-            var fileQuery2 = _fileQuery2.ToArray();
-
-            IEnumerable<FileInfo> _fileQuery3 =
-                from file in fileList3
-                where file.Length > 0
-                orderby file.Name
-                select file;
-
-            var fileQuery3 = _fileQuery3.ToArray();
-
-            IEnumerable<FileInfo> _fileQuery4 =
-                from file in fileList4
-                where file.Length > 0
-                orderby file.Name
-                select file;
-
-            var fileQuery4 = _fileQuery4.ToArray();
-
-            IEnumerable<FileInfo> _fileQuery5 =
-                from file in fileList5
-                where file.Length > 0
-                orderby file.Name
-                select file;
-
-            var fileQuery5 = _fileQuery5.ToArray();
-
-            IEnumerable<FileInfo> _fileQuery6 =
-                from file in fileList6
-                where file.Length > 0
-                orderby file.Name
-                select file;
-
-            var fileQuery6 = _fileQuery6.ToArray();
-
-            IEnumerable<FileInfo> _fileQuery7 =
-                from file in fileList7
-                where file.Length > 0
-                orderby file.Name
-                select file;
-
-            var fileQuery7 = _fileQuery7.ToArray();
-
-            var len1 = Math.Min(20, fileQuery1.Length / 10);
-            var len2 = Math.Min(20, fileQuery2.Length / 10);
-            var len3 = Math.Min(20, fileQuery3.Length / 10);
-            var len4 = Math.Min(20, fileQuery4.Length / 10);
-            var len5 = Math.Min(20, fileQuery5.Length / 10);
-            var len6 = Math.Min(20, fileQuery6.Length / 10);
-            var len7 = Math.Min(20, fileQuery7.Length / 10);
-
+            var len1 = Math.Min(100, fileQuery1.Length / 10);
             var bag1 = Add(new FileInfo[len1], new Random(), fileQuery1);
-            var bag2 = Add(new FileInfo[len2], new Random(), fileQuery2);
-            var bag3 = Add(new FileInfo[len3], new Random(), fileQuery3);
-            var bag4 = Add(new FileInfo[len4], new Random(), fileQuery4);
-            var bag5 = Add(new FileInfo[len5], new Random(), fileQuery5);
-            var bag6 = Add(new FileInfo[len6], new Random(), fileQuery6);
-            var bag7 = Add(new FileInfo[len7], new Random(), fileQuery7);
 
-            return
-                new ConcurrentBag<FileInfo>(bag1.Union(bag2).Union(bag3).
-                Union(bag4).Union(bag5).Union(bag6).Union(bag7)
+            return bag1;
+                
 
-                );
+                
         }
 
     static void Main(string[] args)
